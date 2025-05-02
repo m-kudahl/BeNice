0cbb04ab0c19d74cfce52048c01a9f9423cd87f7
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2025-05-02 12:25:50 +0200

    keyboard library test, word listener implementation

--- BeNice.py
+++ BeNice.py
@@ -1,18 +1,23 @@
 import pandas
-from pynput import keyboard
+import keyboard
 
 words = pandas.read_csv('words.csv')
+replacements = dict(zip(words['badword'], words['goodword']))
 
+def auto_replace():
+    for badword, goodword in replacements.items():
+        def callback(x=badword, y=goodword):
+                for i in range(len(x)+1):
+                     keyboard.send('backspace')
+                keyboard.write(f"{y} ")
 
-replacements = dict(zip(words['badword'],words['goodword']))
+        keyboard.add_word_listener(
+            badword,
+            callback,
+            triggers=['space','enter'],
+            match_suffix=False,
+            timeout=0
+        )
+    keyboard.wait()
 
-
-test_string = "fuck this shit"
-
-def replace_words(input):
-        new_text = input.lower()
-        for badword, goodword in replacements.items():
-                new_text = new_text.replace(badword,goodword)
-        print(new_text)
-
-replace_words(test_string)
+auto_replace()
