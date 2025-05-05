4b0f7a85189e233137d4ed020d1abb26297b2f58
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2025-05-05 21:52:27 +0200

    added some comments before I forget

--- BeNice.py
+++ BeNice.py
@@ -1,16 +1,23 @@
 import pandas
 import keyboard
 
+# read csv into DataFrame
 words = pandas.read_csv('words.csv')
+
+# create dictionary with columns from seperate dataframes to match the values
 replacements = dict(zip(words['badword'], words['goodword']))
 
 def auto_replace():
     for badword, goodword in replacements.items():
+
+        # define callback, needed for keyboard word listener
+        # make sure badword and goodword capture the current values
         def callback(x=badword, y=goodword):
                 for i in range(len(x)+1):
                      keyboard.send('backspace')
                 keyboard.write(f"{y} ")
 
+        # call add_word_listener and use the defined callback on 'badword' trigger
         keyboard.add_word_listener(
             badword,
             callback,
@@ -18,6 +25,7 @@ def auto_replace():
             match_suffix=False,
             timeout=0
         )
+    # wait forever, infinite runtime
     keyboard.wait()
 
 auto_replace()
