a16a803407b1e79c69813e1c146d172ed00d5af9
m-kudahl <115213053+m-kudahl@users.noreply.github.com>
2025-05-06 16:25:01 +0200

    Formatting

--- BeNice.py
+++ BeNice.py
@@ -1,30 +1,36 @@
 import keyboard
 import pandas
 
-words = pandas.read_csv('words.csv')
+words = pandas.read_csv("words.csv")
 
 # create dictionary with columns from seperate dataframes to match the values
-replacements = dict(zip(words['badword'], words['goodword']))
+replacements = dict(zip(words["badword"], words["goodword"]))
 
 # define a letter buffer
-buffer = ''
+buffer = ""
+
 
 def keypress(key):
     global buffer
 
     # reset the buffer on space and enter
-    if key.name == 'space' or key.name == 'enter':
-        buffer = ''  
+    if key.name == "space" or key.name == "enter":
+        buffer = ""
         return
-    
+
     # delete last character from buffer on backspace
-    elif key.name == 'backspace':
+    elif key.name == "backspace":
         buffer = buffer[:-1]
         return
-    
+
     # ugly elif check for combination keys
     # this can be done better
-    elif key.name == 'skift' or key.name == 'shift' or key.name == 'return' or key.name == 'ctrl':
+    elif (
+        key.name == "skift"
+        or key.name == "shift"
+        or key.name == "return"
+        or key.name == "ctrl"
+    ):
         return
 
     # else add letter to buffer
@@ -37,17 +43,18 @@ def keypress(key):
         # force buffer to lowercase to capture uppercase
         # probably not the best way to do this
         if buffer.lower() == badword.lower():
-            
+
             # Delete letters from badword
             for letters in badword:
-                keyboard.send('backspace')
+                keyboard.send("backspace")
 
             # write goodword
             keyboard.write(goodword)
 
             # Reset buffer
-            buffer = ''
+            buffer = ""
+
 
 keyboard.on_press(keypress)
-    # let's wait! (forever)
+# let's wait (forever!)
 keyboard.wait()
